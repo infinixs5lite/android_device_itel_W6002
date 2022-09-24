dd15f52a96304023ecfb61eb5f235ea9959b0239
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-24 17:40:10 +0200

    itel-W6002: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: itel Model: itel W6002 Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_itel-W6002.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_itel-W6002-user \
+    omni_itel-W6002-userdebug \
+    omni_itel-W6002-eng
