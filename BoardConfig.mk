29b9b90a94d3fc6a456d13f22b21f004e6c82886
infinixs5lite <kelvinchinedu97@gmail.com>
2022-09-24 17:42:02 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/itel/itel-W6002
+DEVICE_PATH := device/itel/W6002
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
