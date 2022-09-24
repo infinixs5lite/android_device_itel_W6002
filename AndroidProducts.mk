63832dd9823c4e59c141cabb204bc42fc7538328
infinixs5lite <kelvinchinedu97@gmail.com>
2022-09-24 17:37:25 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_itel-W6002.mk
+    $(LOCAL_DIR)/omni_W6002.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_itel-W6002-user \
-    omni_itel-W6002-userdebug \
-    omni_itel-W6002-eng
+    omni_W6002-eng
