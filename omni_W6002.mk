6f135cef41e433e82c465164c72bf8ad4a77b561
infinixs5lite <kelvinchinedu97@gmail.com>
2024-08-11 00:51:41 +0100

    Update omni_W6002.mk

--- omni_W6002.mk
+++ omni_W6002.mk
@@ -13,8 +13,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, device/itel/W6002/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/pb/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := W6002
